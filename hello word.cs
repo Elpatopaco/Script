fb6f45cdf145dae4624edaee7269451506de797f
Elpatopaco <152917119+Elpatopaco@users.noreply.github.com>
2023-12-04 20:23:59 +0100

    Add files via upload

---
+++ hello word.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helloword : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    { //Esto sirve para que el coso diga Hello world
+        print("Hello,¡ world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
